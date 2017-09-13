6277e50989ad8a9d94ff2d953a12ac95fd285236
arisuaiko <arisuaiko@users.noreply.github.com>
2017-09-13 21:53:32 +0300

    обновлен файл script

--- js/script.js
+++ js/script.js
@@ -1,30 +1,25 @@
-
 var link = document.querySelector(".brown");
-var popup = document.querySelector("modal-search");
-var close = document.querySelector(".brown");
+var popup = document.querySelector(".modal-search");
+var html = document.documentElement;
+
+html.className = html.className.replace("no-js", "js");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-hide");
+  if (popup.classList.contains("modal-show")) {
+    popup.classList.add("modal-hide");
+    popup.classList.remove("modal-show");
+  } else {
+    popup.classList.add("modal-show");
+    popup.classList.remove("modal-hide");
+  }
 });
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-hide");
-});
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-hide")) {
-      popup.classList.remove("modal-hide");
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.add("modal-hide");
     }
   }
 });
 
-/* 
-let div = document.getElementsByClassName('btn brown')[0];
-let modal = document.getElementsByClassName('modal modal-search' [0]);
-
-div.addEventListener('click', function (event) {
-  if (modal.classList[2] == 'hide');
-} else {
-  modal.classList.add('hide');
-})
-*/
